be42f7596365e6214a3a2434872ba843a117a902
Kristian Trenskow <trenskow@me.com>
2015-06-29 00:19:24 +0200

    Added deprecation message.

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ var isvalid = require('isvalid');
 
 module.exports = isvalid.validate;
 module.exports.isvalid = isvalid;
+
+console.error("[DEPRECATED] isvalid-express has been migrated into isvalid. See README.");
